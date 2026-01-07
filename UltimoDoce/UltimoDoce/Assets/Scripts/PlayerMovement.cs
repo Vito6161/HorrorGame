8aab8d44dbfc85986f9c8ebf82f986895d5e0510
bea0707 <vidros_77@protonmail.com>
2026-01-06 23:39:19 -0300

    movimentação e sprites
    movimentação basica funcionando
    
    coloquei os sprites do jogador e dos tiles e fiz uma salinha com os tiles

---
+++ UltimoDoce/UltimoDoce/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Colisão com o chão")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float tamanhoDetector;
+    [SerializeField] private LayerMask layerChão;
+
+    [Header("Valores da movimentação")]
+    [SerializeField] private float velocidade;
+    [SerializeField] private float pulo;
+
+    private Rigidbody2D rb;
+    private float horizontal; 
+
+    private int direção = 1;
+
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if(horizontal > 0.1f && direção < 0 || horizontal < -0.1f && direção > 0)
+        {
+            Virar();
+        }
+        
+        if(Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, pulo);
+        }
+    }
+
+    void Virar()
+    {
+        direção *= -1;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+
+    bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, tamanhoDetector, layerChão);
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * velocidade, rb.linearVelocity.y);
+    }
+}
